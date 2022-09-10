624c0fb39fe2fb2dc7c0fb2720777df0a14d5702
nikita <nikitakuila1611@gmail.com>
2022-09-10 23:21:09 +0530

    addded

--- entities/ground.py
+++ entities/ground.py
@@ -1,18 +1,27 @@
 import pygame
 from util.constants import Constants
 class Ground:
+    Width=37
+    Height=128
     def __init__(self) -> None:
      self.groundImagePath="assets/images/ground.png"
      self.groundImage=pygame.image.load(self.groundImagePath)
-     groundWidth=128
-     groundHeight=37
-     self.groundImage=[self.groundImage.get_rect()for _ in range(Constants.TotalGroundImages)]     
-    counter=0
+     self.images=[self.groundImage.get_rect() for _ in range(Constants.TotalGroundImages)]   
+     self.positions=list()  
+     self.create_ground()
+     
+    def create_ground(self):
+        delta=0
+        for _ in range(Constants.TotalGroundImages):
+         x = Ground.Width/2+ delta
+         y = Constants.HEIGHT- Ground.Height/2
+         self.positions.append((x,y))
+         delta+=Ground.Width
+        
     def display(self,screen):
         for i in range(Constants.TotalGroundImages):
-         groundRect=self.groundImage[i]
-         groundRect.centery=(groundRect.centery)+Constants.WIDTH-self.groundImage 
-         groundRect.centerx=self.groundWidth+ counter
-         screen.blit(self.groundImage[i],groundRect)
-         counter+=self.groundWidth
+         groundRect=self.images[i]
+         groundRect.center=self.positions[i]
+         screen.blit(self.groundImage,groundRect)
+         
         
